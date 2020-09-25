145b2b3b1fc2f2eb4c1a82215ed5460e049e6043
x3 <x3dev@protonmail.com>
2020-09-24 19:20:58 -0500

    rebuilder replace original contracts

--- oz/rebuild.sh
+++ oz/rebuild.sh
@@ -11,10 +11,17 @@ cd ..
 echo 'Cleaning up compiler cache...'
 rm -rf cache
 rm -rf artifacts
+rm -rf contracts.orig
+echo 'Making a backup of the contracts...'
+cp -R contracts contracts.orig
 echo 'Removing log lines...'
 yarn run buidler remove-logs
 echo 'flattening CORE...'
 npx truffle-flattener contracts/CORE.sol >> oz/contracts/CORE.sol
 echo 'flattening CoreVault...'
 npx truffle-flattener contracts/CoreVault.sol >> oz/contracts/CoreVault.sol
+echo 'Removing contracts without logs...'
+rm -rf contracts
+echo 'Putting original contracts back...'
+mv contracts.orig contracts
 echo 'done!'
