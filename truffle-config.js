23d146b363fdff26fe696d80141b2511f91d3b1c
x3 <x3dev@protonmail.com>
2020-09-25 06:01:00 -0500

    cVault Deployers Config

--- truffle-config.js
+++ truffle-config.js
@@ -1,26 +1,49 @@
+const LedgerWalletProvider = require('truffle-ledger-provider');
+const INFURA_APIKEY = process.env.INFURA_APIKEY;
+const kovanLedgerOptions = {
+  networkId: 42, // mainnet
+  // path: "44'/60'/0'/0", // ledger default derivation path
+  path: "44'/60'/0'/0/0", // ledger default derivation path
+  askConfirm: false,
+  accountsLength: 1,
+  accountsOffset: 0,
+};
+
+const kovanProvider = new LedgerWalletProvider(kovanLedgerOptions, `https://kovan.infura.io/v3/${INFURA_APIKEY}`);
+const mainnetProvider = new LedgerWalletProvider(kovanLedgerOptions, `https://mainnet.infura.io/v3/${INFURA_APIKEY}`);
+
 module.exports = {
-  // Uncommenting the defaults below
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
-  // for more details on how to specify configuration options!
-  //
-  //networks: {
-  //  development: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  },
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
-  //}
-  //
+  networks: {
+    development: {
+      protocol: 'http',
+      host: 'localhost',
+      port: 8545,
+      gas: 5000000,
+      gasPrice: 5e9,
+      networkId: '*'
+    },
+    kovan: {
+      provider: kovanProvider,
+      network_id: 42,
+      gas: 4600000,
+    },
+    kovan: {
+      provider: mainnetProvider,
+      network_id: 1,
+      gas: 4600000,
+    }
+  },
   compilers: {
     solc: {
-      version: "0.6.12"
+      version: "0.6.12",
+      docker: false,
+      settings: {
+       optimizer: {
+         enabled: true,
+         runs: 200
+       },
+       evmVersion: "byzantium"
+      }
     }
   }
 };
